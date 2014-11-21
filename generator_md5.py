3b85e3540953f74cdc44f846dd2782507fd21de7
lihaiyan <1730531318@qq.com>
2014-11-21 10:48:27 +0800

    add htc rti contral list

--- generator_md5.py
+++ generator_md5.py
@@ -1,5 +1,5 @@
 import hashlib
 
-content = open("E:\Repo\ADDONS_UPDATE\pre_install_addons.xml").read()
+content = open("E:\Repo\ADDONS_UPDATE\pre_install_addons_htc_rti.xml").read()
 md5 = hashlib.md5(content)
 print md5.hexdigest()
